f9a175b8a121a6737b2f1d78db554e72bf4cca31
Charlie McMahon <charliekmcmahon@gmail.com>
2019-09-17 09:57:20 +1000

    Initial Commit

---
+++ MathsAssessment v1.0/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  MathsAssessment v1.0
+//
+//  Created by Charlie McMahon on 17/9/19.
+//  Copyright © 2019 Charlie x Tom Cash. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <AppleScriptObjC/AppleScriptObjC.h>
+
+int main(int argc, const char * argv[]) {
+    [[NSBundle mainBundle] loadAppleScriptObjectiveCScripts];
+    return NSApplicationMain(argc, argv);
+}
